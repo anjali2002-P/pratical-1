52e8aec6d3f09a4a1efb399461fa58e6ac8249be
Anjali <76858914+anjali2002-P@users.noreply.github.com>
2021-08-13 19:14:38 -0700

    Add files via upload

---
+++ calculator.scala
@@ -0,0 +1,26 @@
+
+
+class MyCalculator {
+  
+        def add(x:Int, y:Int) = x + y;
+        
+        def sub(a: Int, b: Int): Int = a-b
+      
+        def mul(a: Int, b: Int): Long = a*b
+      
+        def div(a: Int, b: Int): Int = 
+        {
+            require(b != 0, "denominator can not be 0")    
+            a/b
+        }
+}
+        
+object Calendar {
+    def main(args: Array[String]) {
+        val calc = new MyCalculator()
+        println("sum of x + y = " + calc.add(25,10));
+        println("sum of x + y = " + calc.sub(25,10));
+        println("sum of x + y = " + calc.mul(25,10));
+        println("sum of x + y = " + calc.div(25,10));
+    }
+}
